1a9a743dbe2c3285eb85f56d2c8a5d81f644ac2b
Eddie Tong <edisonjtong@gmail.com>
2024-08-29 11:52:24 -0600

    animation for selecting answers

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@ let quiz = document.querySelector(".quiz_box");
 let timeLeft = document.querySelector(".timer_sec");
 let nextBtn = document.querySelector(".next_btn");
 let questionText = document.querySelector(".que_text");
-let optionsList = document.querySelector(".options_list");
+let optionsList = document.querySelector(".option_list");
 let questionIndex = document.querySelector(".total_que");
 
 let maxTime = 15;
 let interval;
 let queNum = questionIndex.innerText;
 let choices = [];
+let score = 0;
 
 function showInstructions() {
     instructions.classList.add("activeInfo");
@@ -35,15 +36,37 @@ function exitQuiz() {
 function displayQuestion() {
     let que = `<span> ${questions[queNum].numb}) ${questions[queNum].question}</span>`;
     let opt =
-        `<div class="option"><span>1) ${questions[queNum].options[0]}</span></div>` +
-        `<div class="option"><span>2) ${questions[queNum].options[1]}</span></div>` +
-        `<div class="option"><span>3) ${questions[queNum].options[2]}</span></div>` +
-        `<div class="option"><span>4) ${questions[queNum].options[3]}</span></div>`;
-
-    questionIndex.innerText = ++queNum;
+        `<div class="option"><span>${questions[queNum].options[0]}</span></div>` +
+        `<div class="option"><span>${questions[queNum].options[1]}</span></div>` +
+        `<div class="option"><span>${questions[queNum].options[2]}</span></div>` +
+        `<div class="option"><span>${questions[queNum].options[3]}</span></div>`;
 
     questionText.innerHTML = que;
     optionsList.innerHTML = opt;
+
+    let select = document.querySelectorAll(".option");
+    console.log(select);
+
+    for (let i = 0; i < select.length; i++) {
+        select[i].setAttribute("onclick", "optionSelected(this)");
+        select;
+    }
+
+    questionIndex.innerText = `Question ${++queNum} out of ${questions.length}`;
+    startTimer();
+}
+
+function optionSelected(answer) {
+    let userAns = answer.innerText;
+    let correctAns = questions[queNum - 1].answer;
+    if (userAns === correctAns) {
+        score++;
+        answer.classList.add("correct");
+    } else {
+        answer.classList.add("incorrect");
+    }
+    console.log(correctAns);
+    console.log(userAns);
 }
 
 function startTimer() {

fdc700a2a64ccc9dc3780217e38aeb4b1c9c1bac
Eddie Tong <edisonjtong@gmail.com>
2024-08-29 10:44:38 -0600

    added multiple choice options

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@ let quiz = document.querySelector(".quiz_box");
 let timeLeft = document.querySelector(".timer_sec");
 let nextBtn = document.querySelector(".next_btn");
 let questionText = document.querySelector(".que_text");
-let options = document.querySelector(".options_list");
+let optionsList = document.querySelector(".options_list");
 let questionIndex = document.querySelector(".total_que");
 
 let maxTime = 15;
 let interval;
 let queNum = questionIndex.innerText;
+let choices = [];
 
 function showInstructions() {
     instructions.classList.add("activeInfo");
@@ -32,9 +33,17 @@ function exitQuiz() {
 }
 
 function displayQuestion() {
-    questionText.innerText = questions[queNum].question;
+    let que = `<span> ${questions[queNum].numb}) ${questions[queNum].question}</span>`;
+    let opt =
+        `<div class="option"><span>1) ${questions[queNum].options[0]}</span></div>` +
+        `<div class="option"><span>2) ${questions[queNum].options[1]}</span></div>` +
+        `<div class="option"><span>3) ${questions[queNum].options[2]}</span></div>` +
+        `<div class="option"><span>4) ${questions[queNum].options[3]}</span></div>`;
+
     questionIndex.innerText = ++queNum;
-    startTimer();
+
+    questionText.innerHTML = que;
+    optionsList.innerHTML = opt;
 }
 
 function startTimer() {

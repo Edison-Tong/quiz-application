6f298c2e63b56974640c1502f5777c51dabaaa69
Eddie Tong <edisonjtong@gmail.com>
2024-08-29 08:51:16 -0600

    next button resets timer

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ let instructions = document.querySelector(".info_box");
 let quiz = document.querySelector(".quiz_box");
 let timeLeft = document.querySelector(".timer_sec");
 let nextBtn = document.querySelector(".next_btn");
+let question = document.querySelector(".que_text");
 
 let maxTime = 15;
+let interval;
 
 function showInstructions() {
     instructions.classList.add("activeInfo");
@@ -17,6 +19,8 @@ function showInstructions() {
 function startQuiz() {
     instructions.classList.remove("activeInfo");
     quiz.classList.add("activeQuiz");
+    nextBtn.classList.add("show");
+    displayQuestion();
     startTimer();
 }
 
@@ -24,23 +28,26 @@ function exitQuiz() {
     instructions.classList.remove("activeInfo");
 }
 
-function cycleQuestions() {
+function displayQuestion() {
     console.log("questions");
 }
 
 function nextQuestion() {
-    timeLeft.innerText = maxTime;
+    displayQuestion();
     startTimer();
 }
 
 function startTimer() {
-    let interval = setInterval(() => {
+    clearInterval(interval);
+    timeLeft.innerText = maxTime;
+    interval = setInterval(timer, 250);
+    function timer() {
         timeLeft.innerText--;
         if (timeLeft.innerText == 0) {
+            console.log(0);
             clearInterval(interval);
-            nextBtn.classList.add("show");
         }
-    }, 250);
+    }
 }
 
 startBtn.addEventListener("click", showInstructions);
